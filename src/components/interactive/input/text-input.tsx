06d0fb4d2e9ad8f5e2da32e774ad6a49b75bd479
muffinnxz <mr.muffinnxz@gmail.com>
2024-06-28 18:26:10 +0700

    feat: stable diffusion generate component

---
+++ src/components/interactive/input/text-input.tsx
@@ -0,0 +1,22 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export interface TextInputProps {
+  label: string;
+  value: string;
+  setValue: (value: string) => void;
+}
+
+export default function TextInput({ label, value, setValue }: TextInputProps) {
+  return (
+    <div className="flex flex-col">
+      <Label className="text-lg">{label}</Label>
+      <Input
+        className="mt-2"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Type something here..."
+      />
+    </div>
+  );
+}
