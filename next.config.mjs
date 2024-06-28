5c690c21ecde8bfb68f221df32be9f6790e3c78c
muffinnxz <mr.muffinnxz@gmail.com>
2024-06-28 16:38:40 +0700

    fix: firebase auth config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: `https://reiynai.firebaseapp.com/__/auth/:path*`
+      }
+    ];
+  }
+};
 
 export default nextConfig;
