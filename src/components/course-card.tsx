0fb6eb20c55895c95bf8f41b2e710679816e5a5e
muffinnxz <mr.muffinnxz@gmail.com>
2024-06-29 11:07:40 +0700

    fix: course card image cover

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Course } from "@/interfaces/course";
 import { useRouter } from "@/lib/router-events";
+import Image from "next/image";
 
 const CourseCard = ({ course }: { course: Course }) => {
   const router = useRouter();
@@ -9,8 +10,14 @@ const CourseCard = ({ course }: { course: Course }) => {
   return (
     <div className="bg-white border border-gray-200 rounded drop-shadow p-5 flex flex-col">
       <div className="flex-grow">
-        <div className="h-36 bg-gray-200 flex items-center justify-center mb-5 text-xl">
-          <img src={course.thumbnail} className="h-full w-auto" />
+        <div className="relative h-36 bg-gray-200 flex items-center justify-center mb-5 text-xl rounded-md">
+          <Image
+            src={course.thumbnail}
+            alt={course.name}
+            fill
+            className="h-full w-auto"
+            style={{ objectFit: "cover" }}
+          />
         </div>
         <h3 className="mb-2 text-lg font-semibold">{course.name}</h3>
         <p className="mb-4 text-gray-600">{course.description}</p>
