fe13aa99de0403567ea860fe31c88b0810252ea7
muffinnxz <mr.muffinnxz@gmail.com>
2024-06-30 01:40:30 +0700

    feat: adding openai vision

---
+++ src/pages/api/openai/vision.ts
@@ -0,0 +1,37 @@
+import { NextApiRequestWithUser, firebaseAuth } from "@/middlewares/auth";
+import type { NextApiResponse } from "next";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function handler(req: NextApiRequestWithUser, res: NextApiResponse) {
+  const { prompt, imageUrl } = req.body;
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4-vision-preview",
+      messages: [
+        {
+          role: "user",
+          content: [
+            { type: "text", text: prompt },
+            {
+              type: "image_url",
+              image_url: {
+                url: imageUrl,
+              },
+            },
+          ],
+        },
+      ],
+      max_tokens: 4096,
+    });
+    const result = response.choices[0].message.content;
+    res.status(200).json({ message: "Success", data: result });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export default firebaseAuth(handler);
