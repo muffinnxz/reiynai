16f62aa5463a1d9bef24bf7e40b24bbc439ce1f8
Natum Tongiyang <95202148+Smuggin@users.noreply.github.com>
2024-06-30 08:20:28 +0700

    Add: Typhoon15 examples

--- src/components/interactive/typhoon15-instruct.tsx
+++ src/components/interactive/typhoon15-instruct.tsx
@@ -5,6 +5,7 @@ import TextInput from "./input/text-input";
 import axios from "@/lib/axios";
 import { useToast } from "../ui/use-toast";
 import TextOutput from "./output/text-output";
+import Examples from "./example/example";
 
 export default function TyphoonInstruct() {
   const [input1, setInput1] = useState("");
@@ -48,7 +49,7 @@ export default function TyphoonInstruct() {
         <TextInput key="input-3" label="สินค้าชิ้นที่ 3" value={input3} setValue={setInput3} />
       ]}
       outputs={[<TextOutput key="output-1" label="Generated Text" value={output} />]}
-      example={[<></>]}
+      example={[<Examples key="example-1" src={"https://firebasestorage.googleapis.com/v0/b/reiynai.appspot.com/o/Examples%2FScreenshots%2F%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B9%83%E0%B8%99%E0%B8%A2%E0%B9%88%E0%B8%AD%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%84%E0%B8%B8%E0%B8%93%20(3).png?alt=media&token=147d1733-075d-4f37-acc8-c5ad415dbc61"}/>]}
       onGenerate={onGenerate}
     />
   );
